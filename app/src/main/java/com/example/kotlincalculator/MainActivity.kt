4ffebd23ff5db47955078879857cbe134ebb15ff
SefaUyg <sefauyg32@gmail.com>
2025-04-14 12:36:30 +0300

    Kotlin Calculator
    This calculator created with Kotlin. This calculator is for Android 7.0 or upper.

---
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.kotlincalculator
+
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import com.example.kotlincalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var firstnumber = ""
+    private var currentNumber = ""
+    private var currentOperator = ""
+    private var result = ""
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        //initViews
+        binding.apply {
+            // get all buttons
+            binding.layoutMain.children.filterIsInstance<Button>().forEach { button ->
+                //buttons click listener
+                button.setOnClickListener {
+                    //get clicked button text
+                    val buttonText = button.text.toString()
+                    when{
+                        buttonText.matches(Regex("[0-9]"))->{
+                            if(currentOperator.isEmpty())
+                            {
+                                firstnumber+=buttonText
+                                tvResult.text = firstnumber
+                            }else
+                            {
+                                currentNumber+=buttonText
+                                tvResult.text=currentNumber
+                            }
+                        }
+                        buttonText.matches(Regex("[+\\-*/]"))->{
+                            currentNumber = ""
+                            if (tvResult.text.toString().isNotEmpty())
+                            {
+                                currentOperator = buttonText
+                                tvResult.text = "0"
+                            }
+                        }
+                        buttonText == "="->{
+                            if (currentNumber.isNotEmpty()&& currentOperator.isNotEmpty()){
+                                tvFormula.text = "$firstnumber$currentOperator$currentNumber"
+                                result = evaluateExpression(firstnumber,currentNumber,currentOperator)
+                                firstnumber = result
+                                tvResult.text = result
+                            }
+                        }
+                        buttonText == "."->{
+                            if (currentOperator.isEmpty())
+                            {
+                                if (! firstnumber.contains("."))
+                                {
+                                    if (firstnumber.isEmpty())firstnumber+="0$buttonText"
+                                    else firstnumber += buttonText
+                                    tvResult.text = firstnumber
+                                }
+                            }else
+                            {
+                                if (! currentNumber.contains(".")){
+                                    if (currentNumber.isEmpty()) currentNumber+="0$buttonText"
+                                    else currentNumber += buttonText
+                                    tvResult.text = currentNumber
+                                }
+                            }
+                        }
+                        buttonText == "C"->{
+                            currentNumber = ""
+                            firstnumber = ""
+                            currentOperator = ""
+                            tvResult.text = "0"
+                            tvFormula.text = ""
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun evaluateExpression(firstNumber:String,secondNumber:String,operator:String):String
+    {
+        val num1 = firstNumber.toDouble()
+        val num2 = secondNumber.toDouble()
+        return when(operator)
+        {
+            "+"->(num1+num2).toString()
+            "-"->(num1-num2).toString()
+            "*"->(num1*num2).toString()
+            "/"->(num1/num2).toString()
+            else ->""
+        }
+    }
+}
